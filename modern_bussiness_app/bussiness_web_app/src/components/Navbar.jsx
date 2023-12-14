6fe8548795e0037cd5081dadad2e6e17a144c374
cecilia reynoso <cecireynoso@gmail.com>
2023-12-14 10:43:09 -0300

    navbar

--- modern_bussiness_app/bussiness_web_app/src/components/Navbar.jsx
+++ modern_bussiness_app/bussiness_web_app/src/components/Navbar.jsx
@@ -11,12 +11,26 @@ const Navbar = () => {
 
       <ul className='list-none sm:flex hidden justify-end item-center flex-1 '>
         {navLinks.map((nav, index) => (
-          <li>
-            key={nav.id}
-
+          <li
+          key={nav.id}
+          className={`font-poppins
+          font-normal cursor pointer text-[16px]
+          ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
           </li>
         ))}
       </ul>
+      <div className= 'sm:hidden flex- flex-1 justify-end items-center'>
+        <img src={menu} alt='menu'
+        className='w-[32px] h-[32px]'/>
+
+      </div>
+
+
+
     </nav>
   )
 }
