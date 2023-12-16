d31eec3f11566cdae3b6e6dddccb0d6c411b3cee
cecilia reynoso <cecireynoso@gmail.com>
2023-12-16 18:04:59 -0300

    business component

--- modern_bussiness_app/bussiness_web_app/src/components/Button.jsx
+++ modern_bussiness_app/bussiness_web_app/src/components/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ( ) => {
   return (
-    <div>Button</div>
+    <button type='button'
+    className={`py-4 px-6 bg-blue-gradient font-poppin font-medium rounded-xl
+    text-[18px] text-primary outline-none mt-10`}>
+      Get Started
+
+    </button>
   )
 }
 
