b36861032f193d8e77af267eabc36a90df1ff074
cecilia reynoso <cecireynoso@gmail.com>
2023-12-18 23:23:04 -0300

    business component

--- modern_bussiness_app/bussiness_web_app/src/components/Business.jsx
+++ modern_bussiness_app/bussiness_web_app/src/components/Business.jsx
@@ -4,17 +4,23 @@ import { features } from '../constants';
 import Button from './Button';
 
 const FeatureCard = ( { icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1} ? 'mb-6' : 'mb-0' `}>
-    <div>
-      <img src={ icon } alt='icon'
-      className='w-[50%] h-[50%] object-contain' />
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt='star' className='w-[50%] h-[50%] object-contain' />
+    </div>
+    <div className='flex flex-1 flex-col ml-3' >
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1 '>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-white text-[18px] leading-[23px] mb-1 '>
+        {content}
+      </p>
     </div>
   </div>
+);
 
-)
-
-const Business = () => {
-  return (
+const Business = () => (
     <section id='features'
     className={layout.section}>
       <div className={layout.sectionInfo}>
@@ -25,19 +31,13 @@ const Business = () => {
       </div>
 
       <div className={`${layout.sectionImg} flex-col`}>
-        {features.map((feature, index)=>(
+        {features.map((feature, index) => (
           <FeatureCard key={feature.id} {...feature}
           index={index} />
-
-
-
         ))}
-
-
       </div>
-
     </section>
-  )
-}
+  );
+
 
 export default Business
